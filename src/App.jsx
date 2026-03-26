0953a49bafd447b2b294b2c08158dfa489da3e48
Samakshg11 <samakshgarg2005@gmail.com>
2026-03-26 20:36:46 +0530

    refactor: uncomment input field in Home component

--- src/App.jsx
+++ src/App.jsx
@@ -349,19 +349,14 @@ const Home = () => {
     <>
       <h1>Home Page</h1>
 
-      {/* <input
+      <input
         type="text"
         placeholder="Enter name"
         value={name}
         onChange={(e) => setName(e.target.value)}
-      /> */}
-
-      <input
-        type="text"
-        placeholder="Enter age"
-        value={age}
-        onChange={(e) => setAge(e.target.value)}
       />
+
+     
 
       <br /><br />
 
