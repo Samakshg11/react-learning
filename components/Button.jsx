520a793365c37138153c9a595ae213355e83d4ee
Samakshg11 <samakshgarg2005@gmail.com>
2026-03-29 04:47:43 +0530

    feat: add Button component and update App component to use it

---
+++ components/Button.jsx
@@ -0,0 +1,20 @@
+function Button(props) {
+    const handleClick = () => {
+        alert("button clicked");
+    };
+  return (
+    <button onClick={handleClick}
+      style={{
+        backgroundColor: props.bgColor,
+        color: "white",
+        padding: "10px 20px",
+        border: "none",
+        cursor: "pointer"
+      }}
+    >
+      {props.label}
+    </button>
+  );
+}
+
+export default Button;
--- src/App.jsx
+++ src/App.jsx
@@ -729,34 +729,49 @@
 
 
 
-import { useRef } from "react";
-import "./App.css";
+// import { useRef } from "react";
+// import "./App.css";
 
-const App=()=>{
-  const passwordRef=useRef();
-  const confirmRef=useRef();
+// const App=()=>{
+//   const passwordRef=useRef();
+//   const confirmRef=useRef();
   
-  const handleSubmit=(e)=>{
-    e.preventDefault();
-  if(passwordRef.current.value !== confirmRef.current.value){
-    alert("passwords do not match");
-    return;
-  }
-  alert("Form submitted successfully!");
-  }
-  return(
-    <div className="container">
-      <form onSubmit={handleSubmit}>
-        <h1>Signup Form</h1>
-        <input type="text" placeholder="Enter username" 
-        required minLength="3" maxLength="15" pattern="[A-Za-z0-9]+" title="Only letters and numbers allowed" />
-        <input type="email" placeholder="Enter email" required />
-        <input type="password" placeholder="Enter password" required minLength="6" ref={passwordRef} />
-        <input type="password" placeholder="Confirm password" required minLength="6" ref={confirmRef} />
-        <button type="submit">Register</button>
-      </form>
-    </div>
-  )
+//   const handleSubmit=(e)=>{
+//     e.preventDefault();
+//   if(passwordRef.current.value !== confirmRef.current.value){
+//     alert("passwords do not match");
+//     return;
+//   }
+//   alert("Form submitted successfully!");
+//   }
+//   return(
+//     <div className="container">
+//       <form onSubmit={handleSubmit}>
+//         <h1>Signup Form</h1>
+//         <input type="text" placeholder="Enter username" 
+//         required minLength="3" maxLength="15" pattern="[A-Za-z0-9]+" title="Only letters and numbers allowed" />
+//         <input type="email" placeholder="Enter email" required />
+//         <input type="password" placeholder="Enter password" required minLength="6" ref={passwordRef} />
+//         <input type="password" placeholder="Confirm password" required minLength="6" ref={confirmRef} />
+//         <button type="submit">Register</button>
+//       </form>
+//     </div>
+//   )
 
+// }
+// export default App;
+
+
+//Q=>12
+import Button from "../components/Button";
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <Button label="Click Me" bgColor="blue" />
+      <br /><br />
+      <Button label="Delete" bgColor="red" />
+    </div>
+  );
 }
+
 export default App;
