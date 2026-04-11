fd1a43f9b477e09cd9becef779f527bd8cc669f4
Samakshg11 <samakshgarg2005@gmail.com>
2026-04-11 22:58:38 +0530

    refactor: comment out entire ContactForm component code

--- components/contactForm.jsx
+++ components/contactForm.jsx
@@ -93,41 +93,41 @@
 
 
 
-import React from 'react';
-import {useState} from 'react';
+// import React from 'react';
+// import {useState} from 'react';
 
-export default function ContactForm() {
-    const [name,setName] = useState('');
-    const [email,setEmail] = useState('');
-    const [text,setText] = useState('');
+// export default function ContactForm() {
+//     const [name,setName] = useState('');
+//     const [email,setEmail] = useState('');
+//     const [text,setText] = useState('');
 
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        alert(`Name: ${name}, Email: ${email}, Text: ${text}`);
-    }
-    return (
-        <form onSubmit={handleSubmit}>
-            <input type='text' placeholder ='Enter name' value={name} onChange ={(e) =>setName(e.target.value)}
-            onKeyDown={() => console.log("Key Down")}
-            onKeyUp={() => console.log("Key Up")}
-            onMouseDown={() => console.log("Mouse Down")}
-            />
-            <br></br>
+//     const handleSubmit = (e) =>{
+//         e.preventDefault();
+//         alert(`Name: ${name}, Email: ${email}, Text: ${text}`);
+//     }
+//     return (
+//         <form onSubmit={handleSubmit}>
+//             <input type='text' placeholder ='Enter name' value={name} onChange ={(e) =>setName(e.target.value)}
+//             onKeyDown={() => console.log("Key Down")}
+//             onKeyUp={() => console.log("Key Up")}
+//             onMouseDown={() => console.log("Mouse Down")}
+//             />
+//             <br></br>
 
-            <input type='email' placeholder ='Enter email' value={email} onChange ={(e) =>setEmail(e.target.value)}
-            onKeyDown={() => console.log("Key Down")}
-            onKeyUp={() => console.log("Key Up")}
-            onMouseDown={() => console.log("Mouse Down")}
-            />
-            <br></br>
-            <textarea placeholder='Enter text' value={text} onChange={(e) => setText(e.target.value)}
-            onKeyDown={() => console.log("Key Down")}
-            onKeyUp={() => console.log("Key Up")}
-            onMouseDown={() => console.log("Mouse Down")}
-            />
-            <br></br>
-            <button type='submit' onMouseUp={() => console.log("Mouse Up")}>Submit</button>
+//             <input type='email' placeholder ='Enter email' value={email} onChange ={(e) =>setEmail(e.target.value)}
+//             onKeyDown={() => console.log("Key Down")}
+//             onKeyUp={() => console.log("Key Up")}
+//             onMouseDown={() => console.log("Mouse Down")}
+//             />
+//             <br></br>
+//             <textarea placeholder='Enter text' value={text} onChange={(e) => setText(e.target.value)}
+//             onKeyDown={() => console.log("Key Down")}
+//             onKeyUp={() => console.log("Key Up")}
+//             onMouseDown={() => console.log("Mouse Down")}
+//             />
+//             <br></br>
+//             <button type='submit' onMouseUp={() => console.log("Mouse Up")}>Submit</button>
 
-        </form>
-    )
-}
+//         </form>
+//     )
+// }
