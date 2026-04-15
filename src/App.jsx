1ec42ee11226339d6e91d55bd6807af7e0f9b597
Samakshg11 <samakshgarg2005@gmail.com>
2026-04-15 23:57:06 +0530

    refactor: uncomment useReducer implementation and reducer function in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2371,29 +2371,29 @@
 // }
 
 
-// import { useReducer } from "react";
+import { useReducer } from "react";
 
-// function reducer(state, action) {
-//   switch (action.type) {
-//     case "inc":
-//       return { count: state.count + 1 };
-//     default:
-//       return state;
-//   }
-// }
-
-function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
-
-  return (
-    <>
-      <h1>{state.count}</h1>
-      <button onClick={() => dispatch({ type: "inc" })}>
-        +
-      </button>
-    </>
-  );
+function reducer(state, action) {
+  switch (action.type) {
+    case "inc":
+      return { count: state.count + 1 };
+    default:
+      return state;
+  }
 }
+
+// function App() {
+//   const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+//   return (
+//     <>
+//       <h1>{state.count}</h1>
+//       <button onClick={() => dispatch({ type: "inc" })}>
+//         +
+//       </button>
+//     </>
+//   );
+// }
 
 
 // function App() {
